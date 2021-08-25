b570a077273e1167c5140e83fe1d9add830e085b
slav9n4ik <slava.tresnevv@gmail.com>
2021-08-25 19:01:48 +0300

    init

---
+++ core/src/test/java/ru/lucky/finder/demo/LuckyFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lucky.finder.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
